0b656591326f07a0ed9e3b7c5bd88c137f26d510
RafiaMoazum <rafiamoazum@gmail.com>
2024-06-16 13:43:40 +0500

    corrected  the  folder structure

--- server/config/db.js
+++ server/config/db.js
@@ -11,6 +11,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Wait 5 seconds before timing out
+      socketTimeoutMS: 45000,
     });
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
