e6d251e7f205bfb6d778b5683ccaba34bf3805c8
Nida Fatima <ishaqnida7@gmail.com>
2024-06-21 14:55:32 +0500

    doc branch

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
       >
         <div className="container relative mt-0"> 
           <div className="grid md:grid-cols-2 grid-cols-1 gap-6 items-center">
-            <div>
+          <div>
               <h1 className="font-semibold lg:leading-normal leading-normal tracking-wide text-4xl lg:text-5xl mb-5">
                 Comprehensive Health Solutions for Your Beloved Dogs and Cats
               </h1>
@@ -38,6 +38,7 @@ function Hero() {
                   to="/apply-doctor"
                   className="h-10 px-6 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-teal-500 text-white"
                 >
+  
                   Register as doctor{" "}
                   <i className="mdi mdi-chevron-right ms-1"></i>
                 </Link>
@@ -58,18 +59,18 @@ function Hero() {
                 <div
                   className="absolute md:w-[500px] w-[400px] h-[400px] md:h-[500px] bg-white bottom-1/2 translate-y-1/2 md:left-0 left-1/2 ltr:md:translate-x-0 ltr:-translate-x-1/2 rtl:md:translate-x-0 rtl:translate-x-1/2 -z-10 shadow-md shadow-teal-500/10 rounded-full"
                   style={{ clipPath: "circle(50% at 50% 50%)" }}
-                ></div>
+                  ></div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <About />
-      <Services />
-      <Footer />
-      <Switcher />
-    </>
-  );
-}
-
-export default Hero;
+        </section>
+        <About />
+        <Services />
+        <Footer />
+        <Switcher />
+      </>
+    );
+  }
+  
+  export default Hero;
