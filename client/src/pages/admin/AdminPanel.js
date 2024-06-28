7f67032ac0a3f19525eb2f9cd631ae28e2f7a7eb
Nida Fatima <ishaqnida7@gmail.com>
2024-06-28 13:05:05 +0500

    1

--- client/src/pages/admin/AdminPanel.js
+++ client/src/pages/admin/AdminPanel.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import "../../styles/AdminPanel.css"
+import { useNavigate } from "react-router-dom";
+import "../../styles/AdminPanel.css";
 import Sidebar from "../../components/Sidebar";
 
 const AdminPanel = () => {
-    const navigate = useNavigate();
-  
-    return (
-        
-            <div className="main-layout">
-              <Sidebar opt1="Add Symptoms" link1="/addSymptoms" opt2="Add Diseases" link2="/addDisease" opt3="Add Diet Plan" link3="/addDietPlan"/>
-              <div className="content">
-                <header className="top-nav">
-                  <span className="notification-icon">🔔</span>
-                </header>
-                <main className="main-content">
-                  <h1 className="main-heading">Admin Panel</h1>
-                </main>
-              </div>
-            </div>
-          
-    );
-  
-}
+  const navigate = useNavigate();
 
-export default AdminPanel
+  return (
+    <div className="main-layout">
+      <Sidebar
+        opt1="Add Symptoms"
+        link1="/addSymptoms"
+        opt2="Add Diseases"
+        link2="/addDisease"
+        opt3="Add Diet Plan"
+        link3="/addDietPlan"
+      />
+      <div className="content">
+        <header className="top-nav">
+          <span className="notification-icon">🔔</span>
+        </header>
+        <main className="main-content">
+          <h1 className="main-heading">Admin Panel</h1>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPanel;
